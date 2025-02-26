7c5adefee082ab68cc330cdc556720f339ac3857
Jhonatan Moreira <jhonathanm836@gmail.com>
2025-02-26 00:46:43 -0300

    resolve internal server error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,14 @@
-// app/page.tsx
 import { getPosts } from "@/actions/post.action";
-import { getDbUserId, syncUser } from "@/actions/user.action";
+import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
 import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-import NotFound from "./not-found";
 
 export default async function Home() {
   const user = await currentUser();
-
-  // Verifique se o usuário está autenticado
-  if (!user) {
-    return; // Redireciona para a página de login
-  }
-
-  // Verifique se o usuário já está sincronizado
-  let dbUserId = await getDbUserId();
-  if (!dbUserId) {
-    // Se o usuário não estiver sincronizado, tente sincronizá-lo
-    const syncedUser = await syncUser();
-    if (!syncedUser) {
-      // Se a sincronização falhar, redirecione para uma página de erro
-      return NotFound;
-    }
-    // Após a sincronização, obtenha o ID do usuário novamente
-    dbUserId = await getDbUserId();
-  }
-
-  // Obtenha os posts
   const posts = await getPosts();
+  const dbUserId = await getDbUserId();
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
