a05468bbc0211649148a4f62654f66e75d9bd6c5
Jhonatan Moreira <jhonathanm836@gmail.com>
2025-02-26 00:55:12 -0300

    resolve internal server error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+// app/page.tsx
 import { getPosts } from "@/actions/post.action";
-import { getDbUserId } from "@/actions/user.action";
+import { getDbUserId, syncUser } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
 import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
@@ -7,15 +8,24 @@ import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
   const user = await currentUser();
-  const posts = await getPosts();
   const dbUserId = await getDbUserId();
+
+  // Se o usuário estiver logado mas não estiver sincronizado, sincronize-o
+  if (user && !dbUserId) {
+    await syncUser();
+  }
+
+  // Obtenha os posts
+  const posts = await getPosts();
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
-        {user ? <CreatePost /> : null}
+        {/* Exibe o CreatePost apenas se o usuário estiver logado */}
+        {user && dbUserId ? <CreatePost /> : null}
 
         <div className="space-y-6">
+          {/* Exibe os posts, independentemente de haver um usuário logado */}
           {posts.map((post) => (
             <PostCard key={post.id} post={post} dbUserId={dbUserId} />
           ))}
@@ -23,7 +33,8 @@ export default async function Home() {
       </div>
 
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        <WhoToFollow />
+        {/* Exibe o WhoToFollow apenas se o usuário estiver logado */}
+        {user && dbUserId ? <WhoToFollow /> : null}
       </div>
     </div>
   );
