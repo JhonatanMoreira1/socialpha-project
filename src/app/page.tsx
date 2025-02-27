804a805cd8626d678f23bb3a4d771870061d8da4
Jhonatan Moreira <jhonathanm836@gmail.com>
2025-02-27 18:08:21 -0300

    bugs fixeds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,28 +23,29 @@ export default async function Home() {
 
     // Você pode exibir uma mensagem de erro ou redirecionar para uma página de erro
   } finally {
-    revalidatePath("/");
     const posts = await getPosts();
 
     return (
-      <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-        <div className="lg:col-span-6">
-          {/* Exibe o CreatePost apenas se o usuário estiver logado */}
-          {user && dbUserId ? <CreatePost /> : null}
+      (
+        <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+          <div className="lg:col-span-6">
+            {/* Exibe o CreatePost apenas se o usuário estiver logado */}
+            {user && dbUserId ? <CreatePost /> : null}
 
-          <div className="space-y-6">
-            {/* Exibe os posts, independentemente de haver um usuário logado */}
-            {posts.map((post) => (
-              <PostCard key={post.id} post={post} dbUserId={dbUserId} />
-            ))}
+            <div className="space-y-6">
+              {/* Exibe os posts, independentemente de haver um usuário logado */}
+              {posts.map((post) => (
+                <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+              ))}
+            </div>
+          </div>
+
+          <div className="hidden lg:block lg:col-span-4 sticky top-20">
+            {/* Exibe o WhoToFollow apenas se o usuário estiver logado */}
+            {user && dbUserId ? <WhoToFollow /> : null}
           </div>
         </div>
-
-        <div className="hidden lg:block lg:col-span-4 sticky top-20">
-          {/* Exibe o WhoToFollow apenas se o usuário estiver logado */}
-          {user && dbUserId ? <WhoToFollow /> : null}
-        </div>
-      </div>
+      ) && revalidatePath("/")
     );
   }
 }
