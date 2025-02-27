3ddcad0233f2df563b50fea390c1937bde487e01
Jhonatan Moreira <jhonathanm836@gmail.com>
2025-02-27 15:17:12 -0300

    bugs fixeds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,27 +22,28 @@ export default async function Home() {
     throw new Error("Problema interno, recarrege a página");
 
     // Você pode exibir uma mensagem de erro ou redirecionar para uma página de erro
-  }
-  const posts = await getPosts();
+  } finally {
+    const posts = await getPosts();
 
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-      <div className="lg:col-span-6">
-        {/* Exibe o CreatePost apenas se o usuário estiver logado */}
-        {user && dbUserId ? <CreatePost /> : null}
+    return (
+      <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+        <div className="lg:col-span-6">
+          {/* Exibe o CreatePost apenas se o usuário estiver logado */}
+          {user && dbUserId ? <CreatePost /> : null}
 
-        <div className="space-y-6">
-          {/* Exibe os posts, independentemente de haver um usuário logado */}
-          {posts.map((post) => (
-            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
-          ))}
+          <div className="space-y-6">
+            {/* Exibe os posts, independentemente de haver um usuário logado */}
+            {posts.map((post) => (
+              <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+            ))}
+          </div>
+        </div>
+
+        <div className="hidden lg:block lg:col-span-4 sticky top-20">
+          {/* Exibe o WhoToFollow apenas se o usuário estiver logado */}
+          {user && dbUserId ? <WhoToFollow /> : null}
         </div>
       </div>
-
-      <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        {/* Exibe o WhoToFollow apenas se o usuário estiver logado */}
-        {user && dbUserId ? <WhoToFollow /> : null}
-      </div>
-    </div>
-  );
+    );
+  }
 }
