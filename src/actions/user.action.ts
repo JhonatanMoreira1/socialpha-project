674ea85ebbda2889bb3b6f9f107ef2f4d69a0ebb
Jhonatan Moreira <jhonathanm836@gmail.com>
2025-02-28 00:35:11 -0300

    bugs fixeds

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -43,7 +43,7 @@ export async function syncUser() {
     return dbUser;
   } catch (error) {
     console.error("Error in syncUser:", error);
-    return revalidatePath("/");
+    throw new Error("error in sync user");
   }
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,37 +11,27 @@ import { redirect } from "next/dist/server/api-utils";
 export default async function Home() {
   const user = await currentUser();
   const dbUserId = await getDbUserId();
+  const posts = await getPosts();
+  await syncUser();
+  // Você pode exibir uma mensagem de erro ou redirecionar para uma página de erro
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {/* Exibe o CreatePost apenas se o usuário estiver logado */}
+        {user && dbUserId ? <CreatePost /> : null}
 
-  try {
-    // Se o usuário estiver logado mas não estiver sincronizado, sincronize-o
-    if (user && !dbUserId) {
-      await syncUser();
-    }
-  } catch (error) {
-    console.error("Error in syncUser2:", error);
-
-    // Você pode exibir uma mensagem de erro ou redirecionar para uma página de erro
-  } finally {
-    const posts = await getPosts();
-    return (
-      <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-        <div className="lg:col-span-6">
-          {/* Exibe o CreatePost apenas se o usuário estiver logado */}
-          {user && dbUserId ? <CreatePost /> : null}
-
-          <div className="space-y-6">
-            {/* Exibe os posts, independentemente de haver um usuário logado */}
-            {posts.map((post) => (
-              <PostCard key={post.id} post={post} dbUserId={dbUserId} />
-            ))}
-          </div>
-        </div>
-
-        <div className="hidden lg:block lg:col-span-4 sticky top-20">
-          {/* Exibe o WhoToFollow apenas se o usuário estiver logado */}
-          {user && dbUserId ? <WhoToFollow /> : null}
+        <div className="space-y-6">
+          {/* Exibe os posts, independentemente de haver um usuário logado */}
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
         </div>
       </div>
-    );
-  }
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        {/* Exibe o WhoToFollow apenas se o usuário estiver logado */}
+        {user && dbUserId ? <WhoToFollow /> : null}
+      </div>
+    </div>
+  );
 }
