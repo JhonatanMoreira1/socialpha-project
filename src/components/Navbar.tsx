faf0e5521fbf2f81767a681fcc98e29d689b60d9
Jhonatan Moreira <jhonathanm836@gmail.com>
2025-03-04 00:50:50 -0300

    bugs fixeds

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,6 @@ import { revalidatePath } from "next/cache";
 async function Navbar() {
   const user = await currentUser();
   if (user) await syncUser(); // POST
-  revalidatePath("/");
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
