bf7e3cc12f4e5666b1104af892425ceeb291842d
Jhonatan Moreira <jhonathanm836@gmail.com>
2025-03-09 22:33:53 -0300

    bugs fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default async function Home() {
     const user = await currentUser();
     const dbUserId = await getDbUserId();
     const posts = await getPosts();
+    await syncUser();
 
     // Você pode exibir uma mensagem de erro ou redirecionar para uma página de erro
     return (
